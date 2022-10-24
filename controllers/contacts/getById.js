4de7cbbbf69c138bddb47bfc1537f77f25035889
Ihor Kolontaievskiy <92229337+iSIayer@users.noreply.github.com>
2022-10-24 19:30:05 +0300

    getById del clg

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -2,7 +2,6 @@ const { NotFound } = require("http-errors");
 const { Contact } = require("../../models");
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  console.log(contactId);
   const result = await Contact.findById(contactId);
   if (!result) {
     throw new NotFound(`Контакт з таким id=${contactId} в базі не знайдено!`);
